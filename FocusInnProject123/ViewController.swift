01d31222fb8a3efa9b2fd2063ed612b0ae8f716c
Jeffrey Lipson <s012558@students.lmsd.org>
2019-01-10 13:33:32 -0500

    Initial Commit

---
+++ FocusInnProject123/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FocusInnProject123
+//
+//  Created by Jeffrey Lipson (student LM) on 1/10/19.
+//  Copyright © 2019 Jeffrey Lipson (student LM). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
